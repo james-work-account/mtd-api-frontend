dec2ac673568a207f247d250751bc1eb2be7ae5e
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2019-08-28 13:02:54 +0100

    🦶 🐛

--- scala-frontend/app/connectors/ApiConnector.scala
+++ scala-frontend/app/connectors/ApiConnector.scala
@@ -2,12 +2,13 @@ package connectors
 
 import com.google.inject.ImplementedBy
 import javax.inject.Inject
+import play.api.Logger
 import play.api.libs.json.{JsObject, JsValue}
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.Future
 
-case class IncorrectMethodError(msg: String) extends Exception
+class IncorrectMethodError(msg: String) extends Exception(msg)
 
 @ImplementedBy(classOf[ApiConnectorImpl])
 trait ApiConnector {
@@ -15,11 +16,15 @@ trait ApiConnector {
 }
 
 class ApiConnectorImpl  @Inject()(implicit val ws: WSClient) extends ApiConnector {
-  override def sendRequest(url: String, method: String, body: Option[JsValue], headers: JsObject): Future[JsValue] = {
-    method match {
-      case "GET" | "DELETE" => handleJson(url, headers.as[Map[String, String]].toSeq, method)
-      case "POST" | "PUT" if body.nonEmpty => handleJson(url, headers.as[Map[String, String]].toSeq, method, body.get)
-      case _ => Future.failed(IncorrectMethodError("Unrecognised method: " + method))
+  override def sendRequest(url: String, method: String, optionalBody: Option[JsValue], headers: JsObject): Future[JsValue] = {
+    (method, optionalBody) match {
+      case ("GET" | "DELETE", None) => handleJson(url, headers.as[Map[String, String]].toSeq, method)
+      case ("GET" | "DELETE", Some(_)) =>
+        Logger.logger.warn(s"Unnecessary body sent with $method HTTP verb")
+        handleJson(url, headers.as[Map[String, String]].toSeq, method)
+      case ("POST" | "PUT", Some(body)) => handleJson(url, headers.as[Map[String, String]].toSeq, method, body)
+      case ("POST" | "PUT", None) => throw new IncorrectMethodError(s"Body required for $method HTTP verb")
+      case (_, _) => throw new IncorrectMethodError(s"Unrecognised HTTP verb $method")
     }
   }
 }
